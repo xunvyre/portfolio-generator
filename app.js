02cb785c0793afee08f2abf62ebe162ec3add304
xunvyre <victoria@thedavids.us>
2022-08-08 22:39:24 -0400

    made html creation function and modularized code

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const printProfileData = profileDataArr =>
+/*const printProfileData = profileDataArr =>
 {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);*/
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err =>
+{
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
