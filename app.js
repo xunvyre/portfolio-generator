4f6552e379fc976cecf9b62ae775c45721eab07b
xunvyre <victoria@thedavids.us>
2022-08-09 17:49:24 -0400

    first prompt

--- app.js
+++ app.js
@@ -4,7 +4,9 @@
 };
 
 printProfileData(profileDataArgs);*/
-const fs = require('fs');
+
+const inquirer = require('inquirer');
+/*const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
@@ -13,4 +15,15 @@ fs.writeFile('./index.html', generatePage(name, github), err =>
 {
     if (err) throw new Error(err);
     console.log('Portfolio complete! Check out index.html to see the output!');
-});
+});*/
+
+inquirer
+    .prompt
+    ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
