fb51f3938d47dadd0d056fe96ed58129d3870230
xunvyre <victoria@thedavids.us>
2022-08-10 14:42:20 -0400

    when argument added for about section

--- app.js
+++ app.js
@@ -25,17 +25,58 @@ const promptUser = () =>
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput =>
+            {
+                if (nameInput)
+                {
+                    return true;
+                }
+                else
+                {
+                    console.log('Please enter your name!')
+                    return false;
+                }
+            }
         },
         {
           type: 'input',
           name: 'github',
-          message: 'Enter your GitHub username:'
+          message: 'Enter your GitHub username:',
+          validate: usernameInput =>
+          {
+              if (usernameInput)
+              {
+                  return true;
+              }
+              else
+              {
+                  console.log('Please enter your GitHub username!')
+                  return false;
+              }
+          }
         },
         {
-          type: 'input',
-          name: 'about',
-          message: 'Provide some information about yourself:'
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) =>
+            {
+                if (confirmAbout)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
     ]);
 };
@@ -57,12 +98,36 @@ Add a New Project
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: pnameInput =>
+            {
+                if (pnameInput)
+                {
+                    return true;
+                }
+                else
+                {
+                    console.log('Please enter the name of your project!')
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project:',
+            validate: descInput =>
+            {
+                if (descInput)
+                {
+                    return true;
+                }
+                else
+                {
+                    console.log('Please enter a pro!')
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
